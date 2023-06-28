2145cadbf76f271bbeadce7e3a2da1b725ff2d09
Master Pi <minh.vln140501@gmail.com>
2023-06-28 10:42:22 +0700

    student

--- pages/quiz/[quiz_id].js
+++ pages/quiz/[quiz_id].js
@@ -3,21 +3,24 @@ import React, { useState, useEffect } from "react";
 import { instanceCoreApi } from "@/services/setupAxios";
 import StudentQuiz from "@/components/student/student-quiz";
 import { useRouter } from "next/router";
+
 const API = process.env.NEXT_PUBLIC_API;
 
 export async function getServerSideProps(context) {
     let props = {};
     const quizID = context.params.quiz_id;
-    const IP = context.req.headers['x-forwarded-for'] || context.req.connection.remoteAddress;
 
     try {
         await instanceCoreApi.get(`${API}/quizzes/${quizID}`).then((res) => {
             const quizData = res.data.data;
-            props = {
-                quizDetail: quizData,
-                classDetail: quizData._class,
-                IP: IP
-            }
+            instanceCoreApi.get('https://api.ipify.org/?format=json').then((response) => {
+                const { ip } = response.data;
+                props = {
+                    quizDetail: quizData,
+                    classDetail: quizData._class,
+                    IP: ip
+                }
+            })
         })
     }
     catch (err) {
@@ -46,7 +49,49 @@ const QuizForm = (props) => {
         <Layout pageTitle="Quiz | CNWeb-30">
             <div className="dashboard bg-[#212121] h-screen bg-center bg-cover bg-no-repeat flex items-center">
                 <div className="main-container">
-                    <StudentQuiz IP={props.IP} quizDetail={props.quizDetail} classDetail={props.classDetail} checkLat={latitude} checkLon={longitude} />
+                    {Object.keys(props).length > 0 ? (
+                        props.quizDetail.status === "In Progress" ? (
+                            <StudentQuiz IP={props.IP} quizDetail={props.quizDetail} classDetail={props.classDetail} checkLat={latitude} checkLon={longitude} />
+                        ) : (
+                            <div style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center"
+                            }}>
+                                <h1 style={{
+                                    fontSize: "60px",
+                                    fontWeight: "700"
+                                }}>
+                                    Oh no!
+                                </h1>
+                                <p style={{
+                                    fontWeight: "100",
+                                    fontSize: "20px"
+                                }}>
+                                    Sorry we can only take you half way there, because the server did not want to talk to us lmao.
+                                </p>
+                            </div>
+                        )
+                    ) : (
+                        <div style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center"
+                        }}>
+                            <h1 style={{
+                                fontSize: "60px",
+                                fontWeight: "700"
+                            }}>
+                                Oh no!
+                            </h1>
+                            <p style={{
+                                fontWeight: "100",
+                                fontSize: "20px"
+                            }}>
+                                Sorry we can only take you half way there, because the server did not want to talk to us lmao.
+                            </p>
+                        </div>
+                    )}
                 </div>
             </div>
         </Layout>
