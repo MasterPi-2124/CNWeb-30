4f97f419d7a74912c52bbdf0e5e13b7fb5388895
Master Pi <minh.vln140501@gmail.com>
2023-06-28 17:35:05 +0700

    #24

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,34 @@
 import LogInForm from "@/components/auth/log-in";
 import Layout from "@/components/layout";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import Cookies from "universal-cookie";
+import validToken from "@/services/validToken";
+import { useRouter } from 'next/router';
+
 
 function LoginPage() {
+  const router = useRouter();
+  const cookies = new Cookies();
+  const [token, setToken] = useState(cookies.get("TOKEN"));
+
+  useEffect(() => {
+    const token = cookies.get("TOKEN");
+    if (validToken(token)) {
+      setToken(token);
+      router.push('/dashboard');
+    } else {
+      setToken(null);
+    }
+
+  }, [token]);
+
   return (
     <Layout pageTitle="Log In | CNWeb">
-      <div className="log-in bg-background-1 h-screen bg-center bg-cover bg-no-repeat flex items-center justify-center">
-        <LogInForm />
-      </div>
+      {!token ? (
+        <div className="log-in bg-background-1 h-screen bg-center bg-cover bg-no-repeat flex items-center justify-center">
+          <LogInForm />
+        </div>
+      ) : (<></>)}
     </Layout>
   );
 }
