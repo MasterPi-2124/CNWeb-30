6f72b670250a24cde30ef4c3ff5c22e7681ec401
Master Pi <minh.vln140501@gmail.com>
2023-07-11 07:23:38 +0700

    minot

--- services/securePath.js
+++ services/securePath.js
@@ -1,19 +1,14 @@
 import aesjs from "aes-js";
-import sha256 from "crypto-js/sha256";
 
 const SECRET = process.env.NEXT_PUBLIC_SECRET;
 
 export const encodePath = (path) => {
-    console.log(path)
     const firstEncoded = btoa(path)
     const key = Uint8Array.from(Array.from(SECRET).map(letter => letter.charCodeAt(0)));
     const encodedBytes = aesjs.utils.utf8.toBytes(firstEncoded);
     const aejsCTR = new aesjs.ModeOfOperation.ctr(key);
     const encryptedBytes = aejsCTR.encrypt(encodedBytes);
     const encodedPath = aesjs.utils.hex.fromBytes(encryptedBytes);
-    const sha = sha256(encodedPath);
-    const ddd = aesjs.utils.hex.fromBytes(sha.init.words)
-    console.log(ddd)
     return encodedPath;
 }
 
@@ -24,6 +19,5 @@ export const decodePath = (path) => {
     const decryptedBytes = aejsCTR.decrypt(encryptedBytes);
     const decodedPath = aesjs.utils.utf8.fromBytes(decryptedBytes);
     const finalPath = atob(decodedPath);
-    console.log(finalPath);
     return finalPath;
 }
