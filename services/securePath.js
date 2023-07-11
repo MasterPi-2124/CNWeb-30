d5adb0324d58fa31d03494f9e0be648975203f36
Master Pi <minh.vln140501@gmail.com>
2023-07-11 07:28:15 +0700

    factor

--- services/securePath.js
+++ services/securePath.js
@@ -1,23 +1,13 @@
-import aesjs from "aes-js";
-
-const SECRET = process.env.NEXT_PUBLIC_SECRET;
-
 export const encodePath = (path) => {
-    const firstEncoded = btoa(path)
-    const key = Uint8Array.from(Array.from(SECRET).map(letter => letter.charCodeAt(0)));
-    const encodedBytes = aesjs.utils.utf8.toBytes(firstEncoded);
-    const aejsCTR = new aesjs.ModeOfOperation.ctr(key);
-    const encryptedBytes = aejsCTR.encrypt(encodedBytes);
-    const encodedPath = aesjs.utils.hex.fromBytes(encryptedBytes);
-    return encodedPath;
+    const firstEncoded = btoa(path);
+    const urlEncoded = encodeURIComponent(firstEncoded);
+    const secondEncoded = btoa(urlEncoded);
+    return secondEncoded;
 }
 
 export const decodePath = (path) => {
-    const key = Uint8Array.from(Array.from(SECRET).map(letter => letter.charCodeAt(0)));
-    const encryptedBytes = aesjs.utils.hex.toBytes(path);
-    const aejsCTR = new aesjs.ModeOfOperation.ctr(key);
-    const decryptedBytes = aejsCTR.decrypt(encryptedBytes);
-    const decodedPath = aesjs.utils.utf8.fromBytes(decryptedBytes);
-    const finalPath = atob(decodedPath);
-    return finalPath;
+    const secondDecoded = atob(path);
+    const urlDecoded = decodeURIComponent(secondDecoded);
+    const firstDecoded = atob(urlDecoded);
+    return firstDecoded;
 }
